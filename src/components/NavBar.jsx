905cce0f5dfb96bb35391e85f0e6065b6a15bb32
stoneado <gabrielhuilipan12@gmail.com>
2022-07-19 18:23:42 -0300

    cambie los href del menu

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -10,11 +10,11 @@ const NavBar = () => {
 <nav class="animate__animated animate__fadeInDownBig">
             <ul class="menu">
 
-                <li><a href=".\index.html">Home</a></li>
-                <li> <a href=".\pages\nosotros.html">Nosotros</a></li>
-                <li> <a href=".\pages\galeria.html">Carrito</a></li>
-                <li> <a href=".\pages\contacto.html">Contacto</a></li>
-                <li> <a href=".\pages\comprar.html">Como comprar?</a></li>
+                <li><a href="#">Home</a></li>
+                <li> <a href="#">Nosotros</a></li>
+                <li> <a href="#">Carrito</a></li>
+                <li> <a href="#">Contacto</a></li>
+                <li> <a href="#">Como comprar?</a></li>
 
             </ul>
             
